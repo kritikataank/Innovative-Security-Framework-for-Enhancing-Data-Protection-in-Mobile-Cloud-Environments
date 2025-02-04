645995592a84903dd882c661844f6915f345c1ec
Kritika Taank <taank.kritika@gmail.com>
2025-02-04 12:31:59 +0530

    Delete @types directory

--- @types/present.d.ts
+++
@@ -1,5 +0,0 @@
-// @types/present.d.ts
-declare module 'present' {
-    export function encrypt(message: string, key: Buffer): string;
-    export function decrypt(encryptedMessage: string, key: Buffer): string;
-}
--- @types/simon.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'simon' {
-    export function encrypt(key: Buffer, data: Buffer): Buffer;
-    export function decrypt(key: Buffer, data: Buffer): Buffer;
-}
